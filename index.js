55d546046661b76b90f39b18b6a9259e0c8f6611
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-08 17:23:25 +0300

    feat: add functional of login-popup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav');
-const menuLinks = document.querySelectorAll('.menu-link');
+const navBurgerLinks = document.querySelector('.nav');
+const burgerMenuLinks = document.querySelectorAll('.menu-link');
 
 function open (el, selector) {
   el.classList.add(selector);
@@ -10,77 +10,131 @@ function close (el, selector) {
   el.classList.remove(selector);
 }
 
-function closeBurgerMenu () {
-  close(burger,'active');
-  close(nav,'open');
-  document.removeEventListener('keydown', (ev)=>handleEscape(ev,  closeBurgerMenu));
-  document.removeEventListener('click', (ev)=>handleOut(ev,'.menu-open', '.icon-profile', closeBurgerMenu));
-}
-
-function openBurgerMenu () {
-  open(burger,'active');
-  open(nav,'open');
-  document.addEventListener('keydown', (ev)=> handleEscape(ev, closeBurgerMenu));
-  document.addEventListener('click', (ev)=>handleOut(ev, '.menu-open', '.icon-profile', closeBurgerMenu));
-}
-
-function handleEscape (ev, fn) {
+function handleClickEscape (ev, fn) {
   if(ev.key==="Escape") {
    fn();
   }
 }
-const returnHandleEscape = (ev) => {
-  handleEscape(ev, closeDropMenu)
-}
 
-
-function handleOut (ev, selector1, selector2, fn) {
+function handleClickOut (ev, selector1, selector2, fn) {
   if(!ev.target.closest(selector1) || ev.target.matches(selector2)) {
-    console.log(ev.target);
     fn();
   }
 }
 
-const returnHandleOut = (ev) => {
-  handleOut(ev, '.icon-profile', '.burger', closeDropMenu)
+function handleBurgerClick () {
+  burger.matches('.active') ? closeBurgerMenu() : openBurgerMenu();
+ }
+
+const returnHandleClickEscapeBurger = (ev) => {
+  handleClickEscape(ev, closeBurgerMenu)
 }
 
-function handleBurgerClick () {
- burger.matches('.active') ? closeBurgerMenu() : openBurgerMenu();
+const returnHandleClickOutBurger = (ev) => {
+  handleClickOut(ev, '.menu-open', '.icon-profile',  closeBurgerMenu)
 }
+
+function closeBurgerMenu () {
+  close(burger,'active');
+  close(navBurgerLinks,'open');
+  document.removeEventListener('keydown', returnHandleClickEscapeBurger);
+  document.removeEventListener('click', returnHandleClickOutBurger);
+}
+
+function openBurgerMenu () {
+  open(burger,'active');
+  open(navBurgerLinks,'open');
+  document.addEventListener('keydown', returnHandleClickEscapeBurger);
+  document.addEventListener('click', returnHandleClickOutBurger);
+}
+
+
+
+//слушатели
 
 burger.addEventListener('click', handleBurgerClick);
 
-
-menuLinks.forEach(link => {
+burgerMenuLinks.forEach(link => {
   link.addEventListener('click', () => {
     closeBurgerMenu();
   })
 })
 
-// дроп-менюшки
 
+// дроп-менюшки
 const iconProfile = document.querySelector('.icon-profile');
 const dropMenuLogin = document.querySelector('.drop-menu_login');
 
+const returnHandleClickEscapeDrop = (ev) => {
+  handleClickEscape(ev, closeDropMenu)
+}
+
+const returnHandleClickOutDrop = (ev) => {
+   if (!iconProfile.contains(ev.target) || dropMenuLogin.contains(ev.target)) {
+    closeDropMenu();
+  }
+}
 
 function openDropMenu () {
   open (dropMenuLogin, 'active');
-  document.addEventListener('keydown', returnHandleEscape);
-  document.addEventListener('click', returnHandleOut);
+  document.addEventListener('keydown', returnHandleClickEscapeDrop);
+  document.addEventListener('click', returnHandleClickOutDrop);
 }
 
 function closeDropMenu () {
   close(dropMenuLogin, 'active');
-  document.removeEventListener('keydown', returnHandleEscape);
-  document.removeEventListener('click', returnHandleOut);
+  document.removeEventListener('keydown', returnHandleClickEscapeDrop);
+  document.removeEventListener('click', returnHandleClickOutDrop);
 }
 
 iconProfile.addEventListener('click', openDropMenu);
 
 
-// окно регистрации
+// окно логина
+const profileForm = document.querySelector('.profile-form');
+const profileFormLogin = document.querySelector('.profile-form_type_login');
+const profileCloseButton = document.querySelector('.close-profile-button');
+const profileButtonsLogin = document.querySelectorAll('.profile-button_login');
+const profileButtonsRegister = document.querySelectorAll('.profile-button_register')
+
+function returnHandleClickEscapePopupAuth (ev) {
+  return handleClickEscape(ev,  closeAuthForm)
+}
+
+function returnHandleClickOutPopupAuth (ev) {
+  return handleClickOut(ev,  closeAuthForm)
+}
+
+function openAuthForm () {
+  open(profileFormLogin,'profile-form_opened');
+  document.addEventListener('keydown', returnHandleClickEscapePopupAuth);
+  // document.addEventListener('click', (ev)=>handleOut(ev, '.menu-open', '.icon-profile',  closeAuthForm));
+
+}
+function closeAuthForm () {
+  close(profileFormLogin,'profile-form_opened');
+  document.removeEventListener('keydown', returnHandleClickEscapePopupAuth);
+  // document.removeEventListener('click', (ev)=>handleOut(ev,'.menu-open', '.icon-profile', closeBurgerMenu));
+}
+
+function closeByClick (evt) {
+  if (evt.target.classList.contains("profile-form_opened") || evt.target.classList.contains("close-button")) {
+    closeAuthForm();
+  }
+  }
+
+function openRegisterForm () {
+  console.log('2')
+}
+profileButtonsLogin.forEach((button) => {
+  button.addEventListener('click', openAuthForm)
+})
+
+profileButtonsRegister.forEach((button) => {
+  button.addEventListener('click', openRegisterForm)
+})
+
+profileForm.addEventListener("mousedown", closeByClick);
 
 
 
-console.log('Самооценка: ' + 50 + ' баллов');
